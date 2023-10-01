47a6b67ead885ceb79741e7fd23e142162244002
MaksymBachynskyi <125116688+MaksymBachynskyi@users.noreply.github.com>
2023-10-01 14:49:15 +0300

    first

--- src/components/contactList/contactList.jsx
+++ src/components/contactList/contactList.jsx
@@ -1,16 +1,15 @@
 import { StyledBtn, Wrapper } from './contactList.styled';
+import { deleteContacts } from 'redux/sliceContacts';
+import { useDispatch } from 'react-redux';
 export const ContactList = ({ contacts, onDelete }) => {
+  const dispatch = useDispatch();
   return (
     <div>
       {contacts.map(item => {
         return (
           <Wrapper key={item.id}>
             {item.name}: {item.number}
-            <StyledBtn
-              onClick={() => {
-                onDelete(item.id);
-              }}
-            >
+            <StyledBtn onClick={() => dispatch(deleteContacts(item.id))}>
               Delete
             </StyledBtn>
           </Wrapper>
